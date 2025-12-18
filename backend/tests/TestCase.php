6efd8c32b8280914c068a9a0c9d43311aaf454d4
Hosseinsyd <hosseinsyd997@gmail.com>
2025-12-18 12:59:09 +0330

    feat: create testing database in TestCase setup

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (config('database.default') === 'sqlite') {
+            $path = config('database.connections.sqlite.database');
+
+            if (! file_exists($path)) {
+                touch($path);
+            }
+        }
+    }
 }
